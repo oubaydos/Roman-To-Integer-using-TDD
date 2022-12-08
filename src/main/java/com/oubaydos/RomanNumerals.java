0fc908c78051df868d1ef0fc33561ee1ede9bebd
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-12-08 05:25:36 +0100

    refactor

--- src/main/java/com/oubaydos/RomanNumerals.java
+++ src/main/java/com/oubaydos/RomanNumerals.java
@@ -7,13 +7,14 @@ public class RomanNumerals {
     public int romanToInt(String s) {
         Map<String, Integer> conversionMap = Map.of("I", 1, "X", 10, "L", 50, "C", 100, "D", 500, "M", 1000, "V", 5, "IV", 4);
         int res = 0;
+        char temp;
         for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == 'I' && i < s.length() - 1 && conversionMap.containsKey(s.substring(i, i + 2))) {
+            temp = s.charAt(i);
+            if (temp == 'I' && i < s.length() - 1 && conversionMap.containsKey(s.substring(i, i + 2))) {
                 res += conversionMap.get(s.substring(i, i + 2));
                 i++;
             } else
-                res += conversionMap.getOrDefault(String.valueOf(s.charAt(i)), 0);
+                res += conversionMap.getOrDefault(String.valueOf(temp), 0);
         }
         return res;
     }
