da2dc012849ce2ec468b93e90b40dfa8c367143c
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-12-08 05:16:51 +0100

    test shouldReturn2GivenII passing

--- src/main/java/com/oubaydos/RomanNumerals.java
+++ src/main/java/com/oubaydos/RomanNumerals.java
@@ -5,7 +5,11 @@ import java.util.Map;
 public class RomanNumerals {
 
     public int romanToInt(String s) {
-        Map<String, Integer> conversionMap = Map.of("I", 1, "X", 10, "L", 50, "C", 100, "D", 500,"M",1000);
-        return conversionMap.getOrDefault(s, 5);
+        Map<String, Integer> conversionMap = Map.of("I", 1, "X", 10, "L", 50, "C", 100, "D", 500, "M", 1000, "V", 5);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res += conversionMap.getOrDefault(String.valueOf(s.charAt(i)), 0);
+        }
+        return res;
     }
 }
