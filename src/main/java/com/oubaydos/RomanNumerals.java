2ec2a6a5ece8b08e96bfe225248d79afc1388744
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-12-08 05:02:27 +0100

    added method existence test

---
+++ src/main/java/com/oubaydos/RomanNumerals.java
@@ -0,0 +1,8 @@
+package com.oubaydos;
+
+public class RomanNumerals {
+
+    public int romanToInt(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/oubaydos/RomanNumeralsTest.java
@@ -0,0 +1,15 @@
+package com.oubaydos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class RomanNumeralsTest {
+    @Test
+    public void shouldHaveRomanToInt() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        romanNumerals.romanToInt("");
+    }
+
+
+}
