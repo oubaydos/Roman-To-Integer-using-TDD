3403c1a5a545057bdd0d10c6bd61986ba0d6c990
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-12-08 05:36:54 +0100

    test shouldReturn40GivenXC passing

--- src/main/java/com/oubaydos/RomanNumerals.java
+++ src/main/java/com/oubaydos/RomanNumerals.java
@@ -1,22 +1,26 @@
 package com.oubaydos;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import static javax.swing.UIManager.put;
 
 public class RomanNumerals {
 
     public int romanToInt(String s) {
-        Map<String, Integer> conversionMap = Map.of(
-                "I", 1,
-                "X", 10,
-                "L", 50,
-                "C", 100,
-                "D", 500,
-                "M", 1000,
-                "V", 5,
-                "IV", 4,
-                "IX", 9,
-                "XL", 40
-        );
+        Map<String, Integer> conversionMap = new HashMap<>() {{
+            put("I", 1);
+            put("X", 10);
+            put("L", 50);
+            put("C", 100);
+            put("D", 500);
+            put("M", 1000);
+            put("V", 5);
+            put("IV", 4);
+            put("IX", 9);
+            put("XL", 40);
+            put("XC", 90);
+        }};
         int res = 0;
         char temp;
         for (int i = 0; i < s.length(); i++) {
